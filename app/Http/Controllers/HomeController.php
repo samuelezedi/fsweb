2936c668e27f045d2dfcbf9ecbfdddf4bbdc4476
Japhet Okparah <jalleto4rel@gmail.com>
2020-07-10 22:05:16 +0100

    Routing 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +25,11 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function profile($username)
+    {
+        $user  = User::where('username', $username)->first();
+        return view('profile',compact('user'));
     }
 }
